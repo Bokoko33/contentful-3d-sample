88e752676627beccabb44959ec13032b32cb250d
Bokoko33 <kamabokoman1042@gmail.com>
2022-10-03 15:47:14 +0900

    remove unused sources

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,14 @@
 import 'destyle.css';
 import '~/styles/app.scss';
-import { SampleContextProvider } from '~/context/SampleContext';
+
 import { GlobalHeader } from '~/components/GlobalHeader';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SampleContextProvider>
+    <>
       <GlobalHeader />
       <Component {...pageProps} />
-    </SampleContextProvider>
+    </>
   );
 }
 
